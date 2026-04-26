853f17616930b25e6ea21d0b3e0744ee64edc5f1
AmanGour <compag2022@gmail.com>
2026-04-26 23:58:05 +0530

    feat: added some validation.

--- src/main/java/com/Aman/SecureTaskManagmentAPI/controller/TaskController.java
+++ src/main/java/com/Aman/SecureTaskManagmentAPI/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.Aman.SecureTaskManagmentAPI.controller;
 
 import com.Aman.SecureTaskManagmentAPI.model.Task;
 import com.Aman.SecureTaskManagmentAPI.service.TaskService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +18,22 @@ public class TaskController {
     private TaskService service;
 
     @PostMapping
-    public ResponseEntity<Task>addTask(@RequestBody Task task, Principal principal){
+    public ResponseEntity<Task>addTask(@RequestBody @Valid Task task, Principal principal){
         return new ResponseEntity<>(service.addTask(task,principal.getName()), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Task>> getAllUserTask(Principal principal){
+    public ResponseEntity<List<Task>> getAllUserTask(@Valid Principal principal){
         List<Task> tasks = service.getAllUserTask(principal.getName());
         return ResponseEntity.ok(tasks);
     }
     @DeleteMapping("/id/{id}")
-    public ResponseEntity<String>deleteTask(@PathVariable Integer id,Principal principal){
+    public ResponseEntity<String>deleteTask(@PathVariable @Valid Integer id,Principal principal){
         service.deleteTask(id,principal.getName());
         return ResponseEntity.ok("Task has been deleted successfully");
     }
     @PutMapping("/id/{id}")
-    public ResponseEntity<Task>updateTask(@PathVariable Integer id,Principal principal){
+    public ResponseEntity<Task>updateTask(@PathVariable @Valid Integer id,Principal principal){
 
         return ResponseEntity.ok(service.updateTask(id,principal.getName()));
     }
--- src/main/java/com/Aman/SecureTaskManagmentAPI/model/Users.java
+++ src/main/java/com/Aman/SecureTaskManagmentAPI/model/Users.java
@@ -1,9 +1,13 @@
 package com.Aman.SecureTaskManagmentAPI.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.util.List;
 
@@ -16,11 +20,16 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(nullable = false)
+    @NonNull
+    @NotBlank(message = "Cannot be a blank username")
     private String username;
-    @Column(nullable = false)
+    @NonNull
+    @NotBlank
+    @Email(message = "The email is not in the right format.")
     private String email;
-    @Column(nullable = false)
+    @NonNull
+    @NotBlank
+    @Size(min = 6,max = 20,message = "The password should be in between 6 to 20 characters.")
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
